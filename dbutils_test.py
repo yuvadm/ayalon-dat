02d18c084cd769a620b5845cf536f91a2dde950b
Yuval Adam <yuv.adm@gmail.com>
2010-10-09 18:05:06 +0200

    removed unecessary setUp()

--- dbutils_test.py
+++ dbutils_test.py
@@ -4,9 +4,6 @@ import dbutils
 
 class DBUtilsTest(unittest.TestCase):
 
-    def setUp(self):
-        self.seq = range(10)
-
     def test_get_tables(self):
         tables = dbutils.get_tables()
         self.assertTrue(len(tables) >= 1)
