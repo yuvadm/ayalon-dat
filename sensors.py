64ca78c63f9c021cacd212b0c3341eb5f33b3676
Yuval Adam <yuv.adm@gmail.com>
2010-10-09 17:20:00 +0200

    syntax change

--- sensors.py
+++ sensors.py
@@ -4,6 +4,6 @@ def get_sensor_list():
     from csv import reader
     with open('dtslist.csv', 'r') as f:
         c = reader(f)
-        for row in c:
-            dict[row[0]] = row[2]
+        for id,status,name in c:
+            dict[id] = name
     return dict
