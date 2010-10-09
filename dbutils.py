e046bb92df0ea62cc36626491edbf04811894392
Yuval Adam <yuv.adm@gmail.com>
2010-10-09 17:30:06 +0200

    added get_tables()

--- dbutils.py
+++ dbutils.py
@@ -3,8 +3,8 @@ import dbconfig
 
 connStr = '%s/%s@%s' % (dbconfig.USER, dbconfig.PASS, dbconfig.SID)
 
-with cx_Oracle.connect(connStr) as conn:
-    cur = conn.cursor()
-    cur.execute('SELECT COUNT(*) FROM D_CARRIL_PM_2008_01')
-    for s in cur.fetchall():
-        print s
+def get_tables():
+    with cx_Oracle.connect(connStr) as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT owner, table_name FROM all_tables')
+        return [table for owner, table in cur.fetchall()]
